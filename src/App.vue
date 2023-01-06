3619729253aed6728d3b48a0f82b62d8f9fc84dc
Kirby Smith <kirbsmith@gmail.com>
2023-01-05 19:11:31 -0500

    adjust title size

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-lightgray">
     <div class="flex items-center justify-center bg-graygreen shadow-lg">
-      <router-link :to="{ name : 'Articles'}"><h1 class="text-3xl md:text-5xl py-3 lg:py-8 text-center px-4 font-montserrat font-light"><span class="hidden md:block">Welcome to </span>Zengines Hacker News</h1></router-link>
+      <router-link :to="{ name : 'Articles'}"><h1 class="text-3xl md:text-6xl py-3 lg:py-8 text-center px-4 font-montserrat font-light"><span class="hidden md:inline-block">Welcome to </span> Zengines Hacker News</h1></router-link>
     </div>
     <router-view />
   </div>
