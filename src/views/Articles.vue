e4d505637ef35c75bf37b911c08a0f3068903353
Kirby Smith <kirbsmith@gmail.com>
2023-01-05 19:09:10 -0500

    adjust borders

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -2,7 +2,7 @@
     <div>
       <h2 class="pt-3 md:pt-8 text-2xl text-center font-montserrat">Current Top Stories</h2>
     </div>
-    <div class="flex flex-col items-center md:border-4 border-2 border-black md:mx-8 my-4 md:my-8 shadow-md hover:shadow-2xl rounded-md border-x-0" v-for="article in articles" :key="article">
+    <div class="flex flex-col items-center md:border-4 border-2 border-black mx-4 md:mx-8 my-4 md:my-8 shadow-md hover:shadow-2xl rounded-md" v-for="article in articles" :key="article">
         <a :href="article.data.url" target="_blank"><h3 class="text-center text-lg md:text-3xl font-montserrat font-normal p-2 hover:text-green-800 hover:duration-500" >{{ article.data.title }}</h3></a>
         <p class="text-md md:text-lg md:pt-2 font-montserrat font-light">Author: {{ article.data.by }}</p>
         <p class=" text-sm md:text-lg py-2 md:py-4 font-montserrat font-light">{{ article.data.descendants }} comments</p>
